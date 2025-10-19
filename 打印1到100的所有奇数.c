a58d37594f4c2c5a91f489f9efc66e1d72e67197
ZhouHan20 <3450014365@qq.com>
2025-10-19 11:35:31 +0800

    3道c语言算法题

---
+++ 打印1到100的所有奇数.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//
+//	for (n = 1; n <= 100; n++)
+//	{
+//
+//		if (n % 2 == 1)
+//		{
+//			printf("%d ", n);
+//		}
+//	}
+//	return 0;
+//}
---
+++ 打印乘法口诀表.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	int	b = 1;//两个因数
+//
+//	//printf("a*b=%d",a * b);
+//	//i循环9次合计9行//行循环（外）
+//		 //j循环，i+1次//列循环（内）
+//		//每一次换行a++
+//		//每一次换列b++
+//	int i = 0;
+//	for (i = 0; i < 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < i + 1; j++)
+//		{
+//			printf(" %d * %d = %d ", a,b,a * b);
+//			b++;
+//		}
+//		printf("\n");
+//		b = 1;
+//		a++;
+//	}
+//	return 0;
+//}
